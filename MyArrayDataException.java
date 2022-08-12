f614d56b5c258813f0dd4512d9d6abef6c5dee83
Ramil <nurtdinov.1992@list.ru>
2022-08-12 10:52:43 +0500

    Lesson2Home

---
+++ MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java2.lesson2;
+
+public class MyArrayDataException extends MyArrayExceptions {
+    public MyArrayDataException(String messaage) {
+        super(messaage);
+    }
+}
---
+++ MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java2.lesson2;
+
+public class MyArraySizeException extends MyArrayExceptions {
+    public MyArraySizeException(String messaage) {
+        super(messaage);
+    }
+}
